b00980cb047a370b78d2a1a1d0afce28ff9d7bd4
Devindra <arden.ived@gmail.com>
2019-08-17 09:13:42 +0100

    Unique filenames for each image

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -23,4 +23,24 @@ class ImageUploader < CarrierWave::Uploader::Base
     "#{Rails.root}/tmp/uploads"
   end
 
+  def filename
+    "#{secure_token}.#{file.extension}" if original_filename.present?
+  end
+
+  protected
+
+    def secure_token
+      media_original_filenames_var = :"@#{mounted_as}_original_filenames"
+
+      unless model.instance_variable_get(media_original_filenames_var)
+        model.instance_variable_set(media_original_filenames_var, {})
+      end
+
+      unless model.instance_variable_get(media_original_filenames_var).map{|k,v| k }.include? original_filename.to_sym
+        new_value = model.instance_variable_get(media_original_filenames_var).merge({"#{original_filename}": SecureRandom.uuid})
+        model.instance_variable_set(media_original_filenames_var, new_value)
+      end
+
+      model.instance_variable_get(media_original_filenames_var)[original_filename.to_sym]
+    end
 end
