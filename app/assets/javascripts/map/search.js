7db38b227f36f607fd131f6438e26db45c8f3c5e
Rohit Verma <rohitverma@live.ca>
2019-09-18 23:59:30 -0400

    adding register panel

--- app/assets/javascripts/map/search.js
+++ app/assets/javascripts/map/search.js
@@ -1,13 +1,17 @@
 
 const Search = {
 	infoPanelElement: null,
+  registrationPanelElement: null,
 	boxShadowDivElement: null,
   load() {
     Search.infoPanelElement = document.getElementById("infoPanel");
+    Search.registrationPanelElement = document.getElementById("registrationPanel");
     Search.boxShadowDivElement = document.getElementById("boxShadowDiv");
 
 		document.getElementById('moreInfoLink').addEventListener("click", Search._onMoreInfoLinkClick);
 		document.getElementById('lessInfoLink').addEventListener("click", Search._onLessInfoLinkClick);
+		document.getElementById('cancelRegisterLink').addEventListener("click", Search._onCancelRegisterLinkClick);
+		document.getElementById('registerButton').addEventListener("click", Search._onRegistrationButtonClick);
   },
   _onMoreInfoLinkClick(clickEvent) {
 		clickEvent.preventDefault();
@@ -18,6 +22,16 @@ const Search = {
 		clickEvent.preventDefault();
 		Search._decreaseBoxShadowDiv();
 		Search._toggleDisplayClass(Search.infoPanelElement, "none");
+  },
+  _onCancelRegisterLinkClick(clickEvent) {
+		clickEvent.preventDefault();
+		Search._decreaseBoxShadowDiv();
+		Search._toggleDisplayClass(Search.registrationPanelElement, "none");
+  },
+  _onRegistrationButtonClick(clickEvent) {
+		clickEvent.preventDefault();
+		Search._increaseBoxShadowDiv();
+    Search._toggleDisplayClass(Search.registrationPanelElement, "block");
   },
   _increaseBoxShadowDiv() {
 		Search.boxShadowDivElement.classList.add("width-panel-double");
