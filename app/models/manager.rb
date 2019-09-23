5ca5ae502d264c0153b6e97742f3664d7ceece24
Devindra <arden.ived@gmail.com>
2019-09-23 19:43:48 +0100

    Update detail pages for all record types

--- app/models/manager.rb
+++ app/models/manager.rb
@@ -23,7 +23,10 @@ class Manager < ApplicationRecord
   def managed_by? manager, super_manager: false
     return true if self == manager && !super_manager
     return manager.administrator?
-    # TODO: Allow managers to manage other users within their own regions.
+  end
+
+  def to_s
+    name
   end
 
 end
