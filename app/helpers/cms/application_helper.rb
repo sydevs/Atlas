f50a60fa7996fc79de4668358f0ffa730a94c365
Devindra <arden.ived@gmail.com>
2019-11-03 17:51:35 +0000

    Make breadcrumb urls a bit more intuitive

--- app/helpers/cms/application_helper.rb
+++ app/helpers/cms/application_helper.rb
@@ -21,7 +21,7 @@ module CMS::ApplicationHelper
   end
 
   def menu_item label, record, index: nil, action: nil
-    active = controller_name == index || action_name == index if index
+    active = [controller_name, action_name].include?(index) if index
     active = action_name == action if action
 
     if active
@@ -37,6 +37,18 @@ module CMS::ApplicationHelper
 
   def model_icon model
     content_tag :i, nil, class: "#{MODEL_ICONS[model.table_name.to_sym]} icon"
+  end
+
+  def breadcrumb_url ancestor
+    if action_name == 'index' && policy(ancestor).index_association?(controller_name)
+      url_for([:cms, (ancestor == :worldwide ? nil : ancestor), controller_name])
+    elsif action_name == 'regions' && policy(ancestor).index_association?(:regions)
+      url_for([:cms, (ancestor == :worldwide ? nil : ancestor), :regions])
+    elsif ancestor == :worldwide
+      cms_root_url
+    else
+      url_for([:cms, ancestor])
+    end
   end
 
 end
