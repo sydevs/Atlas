7ba62e8b2a543708d9569b5823007b9b236aa2d2
Devindra <arden.ived@gmail.com>
2019-11-07 09:49:23 +0000

    Attempt to fix assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true # TODO: Disable this
   config.action_controller.perform_caching = true
 
   # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
@@ -25,10 +25,11 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.assets.quiet = true # Make static asset logging quiet
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
