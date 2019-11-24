d59c99d088d1c0e8d835a43fc43ae5b2a584ceaf
Devindra <arden.ived@gmail.com>
2019-11-24 12:22:35 +0000

    Update gcloud bucket

--- config/carrierwave.rb
+++ config/carrierwave.rb
@@ -1,13 +1,13 @@
 CarrierWave.configure do |config|
   config.storage = Rails.env.production? ? :gcloud : :file
   config.asset_host = ActionController::Base.asset_host
-  config.gcloud_bucket = 'sydirectory'
+  config.gcloud_bucket = 'sahaj-atlas'
   config.gcloud_bucket_is_public = true
   config.gcloud_authenticated_url_expiration = 600
 
   config.gcloud_attributes = { expires: 600 }
   config.gcloud_credentials = {
-    gcloud_project: 'sydirectory',
+    gcloud_project: 'sahaj-atlas',
     gcloud_keyfile: JSON.parse(ENV['GOOGLE_CLOUD_KEYFILE']),
   }
 end
