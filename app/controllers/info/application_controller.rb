c31f1e0dcfce3f7ba84b4267683c14b4d856e302
Devindra <arden.ived@gmail.com>
2019-11-26 13:57:20 +0000

    Info page now recognizes logged in users

--- app/controllers/info/application_controller.rb
+++ app/controllers/info/application_controller.rb
@@ -1,9 +1,18 @@
 class Info::ApplicationController < ActionController::Base
 
+  include Passwordless::ControllerHelpers
+  helper_method :current_user
+
   def about
   end
 
   def statistics
   end
+
+  private
+
+    def current_user
+      @current_user ||= authenticate_by_session(Manager)
+    end
 
 end
