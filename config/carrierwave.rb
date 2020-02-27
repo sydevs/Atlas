d5065c13be36d8a87c30e4f30f167e3676b1e512
Devindra <arden.ived@gmail.com>
2020-02-27 16:58:26 +0000

    Fix carrierwave configuration

--- config/carrierwave.rb
+++ config/carrierwave.rb
@@ -1,13 +1,19 @@
 CarrierWave.configure do |config|
-  config.storage = Rails.env.production? ? :gcloud : :file
   config.asset_host = ActionController::Base.asset_host
-  config.gcloud_bucket = 'sahaj-atlas'
-  config.gcloud_bucket_is_public = true
-  config.gcloud_authenticated_url_expiration = 600
 
-  config.gcloud_attributes = { expires: 600 }
-  config.gcloud_credentials = {
-    gcloud_project: 'sahaj-atlas',
-    gcloud_keyfile: JSON.parse(ENV['GOOGLE_CLOUD_KEYFILE']),
-  }
+  if ENV['GCLOUD_BUCKET'].present?
+    config.storage = :gcloud
+    config.asset_host = "https://#{ENV['GCLOUD_BUCKET']}" if ENV['GCLOUD_BUCKET'].include?('.')
+    config.gcloud_bucket = ENV['GCLOUD_BUCKET']
+    config.gcloud_bucket_is_public = true
+    config.gcloud_authenticated_url_expiration = 600
+
+    config.gcloud_attributes = { expires: 600 }
+    config.gcloud_credentials = {
+      gcloud_project: 'sahaj-atlas',
+      gcloud_keyfile: ENV['GOOGLE_CLOUD_KEYFILE'].present? ? JSON.parse(ENV['GOOGLE_CLOUD_KEYFILE']) : nil,
+    }
+  else
+    config.storage = :file
+  end
 end
