e2e66e7fc320ac631c1f7049661c07d358de85d9
Devindra <arden.ived@gmail.com>
2020-08-14 11:43:57 +0100

    Allow forced syncing for testing

--- app/controllers/cms/application_controller.rb
+++ app/controllers/cms/application_controller.rb
@@ -160,7 +160,7 @@ class CMS::ApplicationController < ActionController::Base
     else
       if params[:force] == 'true'
         render 'cms/application/sync'
-      elsif !MapboxSync.needs_sync?
+      elsif false && !MapboxSync.needs_sync? # TODO: Remove this "false" after finished testing
         redirect_to cms_root_url, flash: { notice: translate('cms.messages.sync.not_needed') }
       elsif !MapboxSync.can_sync?
         redirect_to cms_root_url, flash: { error: translate('cms.messages.sync.not_allowed', time_ago: helpers.time_ago_in_words(MapboxSync.active_sync_at), time_from_now: helpers.time_ago_in_words(MapboxSync.can_sync_at)) }
