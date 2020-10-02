43f5efdcfec76a782f624e12f0b1dc8acfce4c08
Devindra <arden.ived@gmail.com>
2020-10-02 14:11:45 +0100

    Fix venue query

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -25,8 +25,8 @@ class Venue < ApplicationRecord
   # Scopes
   scope :publicly_visible, -> { published.has_events }
   scope :mappable, -> { published.has_mappable_events }
-  scope :has_events, -> { joins(:events).where('events.published = true AND events.updated_at > ?', Expirable.expire_date) }
-  scope :has_mappable_events, -> { joins(:events).where('events.published = true AND events.updated_at > ? AND events.online <> true', Expirable.expire_date) }
+  scope :has_events, -> { joins(:events).where('events.published IS true AND events.updated_at > ?', Expirable.expire_date) }
+  scope :has_mappable_events, -> { joins(:events).where('events.published IS true AND events.updated_at > ? AND events.online IS NOT true', Expirable.expire_date) }
 
   # Delegations
   delegate :all_managers, :managed_by?, to: :parent
