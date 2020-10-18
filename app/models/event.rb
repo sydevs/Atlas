4964f8852c11a4e83fe1bbaa6b7fcd40b4a967a7
Devindra <arden.ived@gmail.com>
2020-10-18 16:03:45 +0100

    Fix manager invite

--- app/models/event.rb
+++ app/models/event.rb
@@ -20,7 +20,7 @@ class Event < ApplicationRecord
 
   belongs_to :manager
   accepts_nested_attributes_for :manager
-  before_validation :find_or_create_manager
+  after_save :find_or_create_manager
 
   enum category: { intro: 1, intermediate: 2, course: 3, public_event: 4, concert: 5 }
   enum recurrence: { day: 0, monday: 1, tuesday: 2, wednesday: 3, thursday: 4, friday: 5, saturday: 6, sunday: 7 }
