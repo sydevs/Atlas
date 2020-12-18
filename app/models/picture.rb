0030ecbff1b8427c49ed93271e06e3719e29a9e1
Devindra <arden.ived@gmail.com>
2020-12-18 13:31:15 -0800

    Fix permission error for managing pictures

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,6 +3,9 @@ class Picture < ApplicationRecord
   # Extensions
   mount_uploader :file, ImageUploader
 
+  # Associations
+  belongs_to :parent, polymorphic: true
+
   # Methods
 
   def managed_by? manager, super_manager: nil # rubocop:disable Link/UnusedMethodArgument
