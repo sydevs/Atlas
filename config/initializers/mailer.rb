7cdb3b33dff31c8aa5d72d29d696d32ef283f4a3
Devindra <arden.ived@gmail.com>
2021-05-03 15:41:00 -0700

    Change smtp port

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.gmail.com',
-    port: 587,
+    port: 465,
     user_name: ENV['SMTP_EMAIL'],
     password: ENV['SMTP_PASSWORD'],
     domain: 'sydevelopers.com',
