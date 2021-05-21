308254432f5abec0500f48c00a61ad8d54942486
Devindra <arden.ived@gmail.com>
2021-05-20 18:00:08 -0700

    Remove asset host from carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,7 +8,7 @@ CarrierWave.configure do |config|
     # while "assets.wemeditate.co" is treated as a host with URL's like:
     # https://assets.wemeditate.co/uploads/<carrier-wave-path>
     config.gcloud_bucket = ENV['GCLOUD_BUCKET']
-    config.asset_host = "https://#{ENV['GCLOUD_BUCKET']}" if ENV['GCLOUD_BUCKET'].include?('.')
+    # config.asset_host = "https://#{ENV['GCLOUD_BUCKET']}" if ENV['GCLOUD_BUCKET'].include?('.')
     config.gcloud_bucket_is_public = true
     config.gcloud_authenticated_url_expiration = 600
     config.gcloud_attributes = { expires: 600 }
