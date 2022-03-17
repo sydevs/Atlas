fdcccb90034dcb73cb48adf146a542fd75f76442
Devindra <arden.ived@gmail.com>
2022-03-17 14:08:38 +0100

    Fix client creation

--- app/controllers/api/wix_controller.rb
+++ app/controllers/api/wix_controller.rb
@@ -2,21 +2,24 @@ class API::WixController < API::ApplicationController
   skip_before_action :authenticate_client!
 
   def auth
-    @client = Client.new
     redirect_to "https://www.wix.com/installer/install?#{{
       token: params[:token],
       appId: ENV.fetch('WIX_APP_ID'),
       redirectUrl: api_wix_setup_url,
-      state: @client.id
     }.to_query}"
   end
 
   def setup
-    auth_code = params[:code]
-    @client = Client.find(params[:state])
-    @client.wix_id = params[:instanceId]
-    tokens = WixAPI.get_tokens(auth_code, auth: true)
-    @client.update_column refresh_token: tokens['refresh_token']
+    puts "SETUP #{params.pretty_inspect}"
+    tokens = WixAPI.get_tokens(params[:code], auth: true)
+    @client = Client.create({
+      label: "Wix App #{params[:instanceId]}"
+      secret_key: SecureRandom.uuid,
+      public_key: SecureRandom.uuid,
+      wix_id: params[:instanceId],
+      refresh_token: tokens['refresh_token'],
+    })
+
     WixAPI.close_window(tokens['access_token'])
   end
 
