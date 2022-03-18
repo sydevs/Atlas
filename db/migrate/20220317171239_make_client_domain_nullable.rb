871fa59cab37fc0c53c788ddb90e5ae6d06386f1
Devindra <arden.ived@gmail.com>
2022-03-18 11:56:26 +0100

    Add change_column from/to

--- db/migrate/20220317171239_make_client_domain_nullable.rb
+++ db/migrate/20220317171239_make_client_domain_nullable.rb
@@ -1,6 +1,6 @@
 class MakeClientDomainNullable < ActiveRecord::Migration[6.1]
   def change
     change_column_null :clients, :domain, true
-    change_column_default :clients, :domain, nil
+    change_column_default :clients, :domain, from: "", to: nil
   end
 end
