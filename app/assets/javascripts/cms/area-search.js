2f28dbf0157ce106ee471a9b14730d93f29092b4
Devindra <arden.ived@gmail.com>
2022-08-04 22:34:30 +0100

    Refactor area search

--- app/assets/javascripts/cms/area-search.js
+++ app/assets/javascripts/cms/area-search.js
@@ -1,27 +1,9 @@
-/* global $, RegionMap */
-/* exported GeoSearch */
+/* global $ */
+/* exported AreaSearch */
 
-const GeoSearch = {
-  load: function() {
-    this.$search = $('.ui.area.search')
-    if (this.$search.length > 0) {
-      this.mode = 'area'
-      this.loadAreaSearch()
-      return
-    }
-
-    // Unfortunately this doesn't return consistent results, and so is currently disabled
-    /*
-    this.$search = $('.ui.venue.search')
-    if (this.$search.length > 0) {
-      this.mode = 'venue'
-      this.loadVenueSearch()
-      return
-    }
-    */
-  },
-
-  loadAreaSearch: function() {
+const AreaSearch = {
+  load() {
+    this.$search = $('#js-area-search')
     this.$name = $('#area_name')
     this.$latitude = $('#area_latitude')
     this.$longitude = $('#area_longitude')
@@ -35,30 +17,6 @@ const GeoSearch = {
       minCharacters: 3,
       apiSettings: {
         url: `/cms/areas/geosearch?country=${this.$search.data('country')}&query={query}`,
-      },
-      onSelect: (result, _response) => {
-        this.$name.val(result.structured_formatting.main_text)
-        this.$search.search('set value', '')
-        this.$search.search('display message', 'Retrieving data', 'message')
-        this.fetchGeometry(result.place_id)
-      }
-    })
-  },
-
-  loadVenueSearch: function() {
-    this.$name = $('#venue_name')
-    this.$street = $('#venue_street')
-    this.$city = $('#venue_city')
-    this.$province = $('#venue_province_code')
-    this.$country = $('#venue_country_code')
-    this.$postcode = $('#venue_postcode')
-
-    const country = this.$search.data('country')
-
-    this.$search.search({
-      minCharacters: 3,
-      apiSettings: {
-        url: country ? `/cms/venues/geosearch?country=${country}&query={query}` : '/cms/venues/geosearch?query={query}',
       },
       onSelect: (result, _response) => {
         this.$name.val(result.structured_formatting.main_text)
@@ -111,4 +69,8 @@ const GeoSearch = {
   }
 }
 
-$(document).on('ready', function() { GeoSearch.load() })
+$(document).on('ready', function() {
+  if ($('#js-area-search').length > 0) {
+    AreaSearch.load()
+  }
+})
