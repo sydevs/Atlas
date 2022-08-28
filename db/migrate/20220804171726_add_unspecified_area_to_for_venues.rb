c0d4f49e6e71e011ac9cba949087b20b372d57e7
Devindra <arden.ived@gmail.com>
2022-08-28 09:02:08 +0100

    Clear migration

--- db/migrate/20220804171726_add_unspecified_area_to_for_venues.rb
+++ db/migrate/20220804171726_add_unspecified_area_to_for_venues.rb
@@ -1,19 +1,4 @@
 class AddUnspecifiedAreaToForVenues < ActiveRecord::Migration[6.1]
   def up
-    Country.all.each do |country|
-      events = country.events.where(local_area_id: nil).where.not(venue_id: nil)
-      puts "No missing local areas in #{country.country_code}" && next unless events.present?
-
-      local_area = LocalArea.create_with({
-        country_code: country.country_code,
-        latitude: 51.477928,
-        longitude: -0.001545,
-        radius: 0,
-        time_zone: "Europe/London",
-      }).find_or_create_by!(name: "Unspecified Area")
-
-      events.update_all(local_area_id: local_area.id)
-      puts "Add #{events.count} event(s) to Unspecified Area, #{country.country_code} (#{local_area.id})"
-    end
   end
 end
