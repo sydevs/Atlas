dd3207c7e2608bffc7053192b89f0b2934b7393a
Devindra <arden.ived@gmail.com>
2022-09-09 11:34:56 +0100

    Update embed.js

--- app/views/map/embed.js
+++ app/views/map/embed.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
   const params = new URLSearchParams(location.search)
   const script = document.currentScript
   const iframe = document.createElement('IFRAME')
-  iframe.src = `https://${location.host}/map${script.dataset.path}?key=${script.dataset.key}`
+  iframe.src = `https://${location.host}/map${script.getAttribute('path')}?key=${script.getAttribute('key')}`
   iframe.width = '100%'
   iframe.border = 0
   iframe.addEventListener('load', function() {
