acb3b7bdf70b99eb8eee28365e0a6e5638f8bdfd
Devindra <arden.ived@gmail.com>
2022-09-26 11:22:46 +0200

    Fix migration error

--- db/migrate/20220926085811_remove_client_domain_column.rb
+++ db/migrate/20220926085811_remove_client_domain_column.rb
@@ -1,6 +1,7 @@
 class RemoveClientDomainColumn < ActiveRecord::Migration[6.1]
   def change
     Client.in_batches.each_record do |client|
+      client.config ||= {}
       client.config['domain'] = client[:domain]
       client.save! validate: false
     end
