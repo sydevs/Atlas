15abc191a29e9fb2a925c9da9ab417b32be464e1
Devindra <arden.ived@gmail.com>
2022-09-26 11:18:00 +0200

    Fix client config errors

--- app/models/client.rb
+++ app/models/client.rb
@@ -9,6 +9,8 @@ class Client < ApplicationRecord
     summary_email_sent_at
   ]
 
+  store_accessor :config, :embed_type, :routing_type, :default_view, :domain, :locale
+
   enum embed_type: %i[iframe script url], _suffix: 'embed'
   enum routing_type: %i[query path], _suffix: 'routing'
   enum default_view: %i[map list], _suffix: 'view'
@@ -17,8 +19,9 @@ class Client < ApplicationRecord
   belongs_to :location, polymorphic: true, optional: true
 
   # Validations
-  validates_presence_of :label, :public_key, :secret_key
-  validate :validate_config
+  before_validation -> { self.location_id = nil }, unless: :location_type?
+  validates_presence_of :label, :public_key, :secret_key, :domain
+  validates :domain, format: { with: /[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?/ }
 
   # Scopes
   default_scope { order(last_accessed_at: :desc, updated_at: :desc) }
@@ -31,28 +34,8 @@ class Client < ApplicationRecord
     location || nil
   end
 
-  def domain
-    domain = config&.dig('domain')
-    domain if domain.present?
-  end
-
-  def locale
-    locale = config&.dig('locale')
-    locale.present? ? locale : 'en'
-  end
-
   def url
     "https://#{domain || "wemeditate.com#{"/" + locale if locale != 'en'}"}/map"
   end
-
-  private
-
-    def validate_config
-      self.location_id = nil unless location_type.present?
-
-      errors.add(:location_id, :invalid) if location_id.present? && location.nil?
-      # errors.add(:domain, :blank) unless config['domain'].present?
-      errors.add(:domain, :invalid) unless !config['domain'].present? || config['domain'].match(/^[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?$/)
-    end
 
 end
---
+++ db/migrate/20220926085811_remove_client_domain_column.rb
@@ -0,0 +1,10 @@
+class RemoveClientDomainColumn < ActiveRecord::Migration[6.1]
+  def change
+    Client.in_batches.each_record do |client|
+      client.config['domain'] = client[:domain]
+      client.save! validate: false
+    end
+
+    remove_column :clients, :domain, :string
+  end
+end
