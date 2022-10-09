acb5a92d731d924ec29f8cd7edef991f79f6da59
Devindra <arden.ived@gmail.com>
2022-10-09 19:30:24 +0100

    Allow emails to be translated and setup reminder

--- app/controllers/concerns/sendinblue_api.rb
+++ app/controllers/concerns/sendinblue_api.rb
@@ -10,6 +10,7 @@ module SendinblueAPI
 
   TEMPLATES = {
     confirmation: 9,
+    reminder: 12,
   }.freeze
 
   def self.subscribe email, list_id, attributes
@@ -26,11 +27,89 @@ module SendinblueAPI
     puts "Exception when calling ContactsApi->create_contact: #{e} #{e.response_body}"
   end
 
-  def self.send_email config
+  def self.send_email template, config
+    config.reverse_merge!({
+      templateId: SendinblueAPI::TEMPLATES[template],
+      # sender: { name: 'We Meditate', email: 'admin@wemeditate.com' },
+      params: {},
+      tags: [],
+    })
+
+    config[:tags] << 'atlas'
+    config[:tags] << template.to_s
+
+    config[:params][:text].reverse_merge!(I18n.translate('emails.common'))
+    pp config
+
     client = SibApiV3Sdk::TransactionalEmailsApi.new
     p client.send_transac_email(config)
   rescue SibApiV3Sdk::ApiError => e
     puts "Exception when calling TransactionalEmailsApi->send_transac_email: #{e} #{e.response_body}"
+  end
+
+  def self.send_confirmation_email registration
+    registration = registration.extend(RegistrationDecorator)
+    event = registration.event.extend(EventDecorator)
+    scope = "emails.confirmation.#{event.layer}"
+
+    text = {
+      header: I18n.translate('header', scope: scope, name: registration.first_name)
+    }
+
+    %i[subheader invite_a_friend get_directions faqs].each do |field|
+      text[field] = I18n.translate(field, scope: scope)
+    end
+
+    SendinblueAPI.send_email(:confirmation, {
+      subject: I18n.translate('subject', scope: scope, event_name: event.label),
+      to: [{ name: registration.name, email: registration.email }],
+      params: {
+        name: registration.first_name,
+        url: event.map_url,
+        label: event.label,
+        address: event.address,
+        timing: [event.start_time, event.end_time].compact.join(' - '),
+        date: registration.starting_date.to_s(:short),
+        weekday: registration.starting_at_weekday.upcase,
+        directions_url: event.decorated_venue&.directions_url,
+        text: text,
+      },
+    })
+  end
+
+  def self.schedule_reminder_email registration
+    registration = registration.extend(RegistrationDecorator)
+    event = registration.event.extend(EventDecorator)
+    scope = "emails.reminder.#{event.layer}"
+
+    text = {
+      header: I18n.translate('header', scope: scope, name: registration.first_name)
+    }
+
+    %i[subheader action].each do |field|
+      text[field] = I18n.translate(field, scope: scope)
+    end
+
+    schedule_at = registration.starting_at - (event.online? ? 1.hour : 1.day)
+    schedule_at = 1.minute.from_now if event.online? && schedule_at < Time.now
+    schedule_at = 1.minute.from_now # For development
+    return if schedule_at < Time.now
+
+    SendinblueAPI.send_email(:reminder, {
+      scheduledAt: schedule_at.utc.iso8601,
+      subject: I18n.translate('subject', scope: scope, event_name: event.label),
+      to: [{ name: registration.name, email: registration.email }],
+      params: {
+        name: registration.first_name,
+        label: event.label,
+        address: event.address,
+        timing: [event.start_time, event.end_time].compact.join(' - '),
+        date: registration.starting_date.to_s(:short),
+        weekday: registration.starting_at_weekday.upcase,
+        link: event.online? ? event.online_url : event.decorated_venue&.directions_url,
+        text: text,
+      },
+    })
   end
 
 end
--- app/graphql/mutations/create_registration.rb
+++ app/graphql/mutations/create_registration.rb
@@ -28,7 +28,7 @@ class Mutations::CreateRegistration < Mutations::BaseMutation
 
     registration = Registration.joins(:event).find_or_initialize_by(arguments)
 
-    if !registration.new_record?
+    if !registration.new_record? && !Rails.env.development?
       {
         status: 'info',
         message: I18n.translate('map.registration.feedback.duplicate'),
@@ -36,51 +36,14 @@ class Mutations::CreateRegistration < Mutations::BaseMutation
         registration: registration,
       }
     elsif registration.save
-      registration = registration.extend(RegistrationDecorator)
-      event = registration.event.extend(EventDecorator)
-
       SendinblueAPI.subscribe(registration.email, SendinblueAPI::LISTS[:registrations], {
         email: registration.email,
         firstname: registration.first_name,
         lastname: registration.last_name,
       })
 
-      SendinblueAPI.send_email({
-        sender: { name: 'We Meditate', email: 'admin@wemeditate.com' },
-        to: [{ name: registration.name, email: registration.email }],
-        subject: "Registratin Confirmed - #{registration.event.label}",
-        templateId: SendinblueAPI::TEMPLATES[:confirmation],
-        params: {
-          url: event.map_url,
-          label: event.label,
-          address: event.address,
-          timing: [event.start_time, event.end_time].compact.join(' - '),
-          date: registration.starting_date.to_s(:short),
-          weekday: registration.starting_at_weekday.upcase,
-          directions_url: event.decorated_venue&.directions_url
-        },
-        tags: %w[atlas],
-      })
-      
-      if registration.starting_at > 1.day.from_now
-        SendinblueAPI.send_email({
-          scheduledAt: (registration.starting_at - 1.day).utc.iso8601,
-          sender: { name: 'We Meditate', email: 'admin@wemeditate.com' },
-          to: [{ name: registration.name, email: registration.email }],
-          subject: "Starting Soon - #{registration.event.label}",
-          templateId: SendinblueAPI::TEMPLATES[:confirmation],
-          params: {
-            url: event.map_url,
-            label: event.label,
-            address: event.address,
-            timing: [event.start_time, event.end_time].compact.join(' - '),
-            date: registration.starting_date.to_s(:short),
-            weekday: registration.starting_at_weekday.upcase,
-            directions_url: event.decorated_venue&.directions_url
-          },
-          tags: %w[atlas reminder],
-        })
-      end
+      SendinblueAPI.send_confirmation_email(registration)
+      SendinblueAPI.schedule_reminder_email(registration)
 
       {
         status: 'success',
