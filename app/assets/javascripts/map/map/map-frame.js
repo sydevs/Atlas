c83a7df9dd21b5cb634d03725d528eb7fa7dbdd9
Devindra <devindra.payment@gmail.com>
2022-11-26 18:28:32 -0400

    Fix fitBounds on mobile

--- app/assets/javascripts/map/map/map-frame.js
+++ app/assets/javascripts/map/map/map-frame.js
@@ -221,7 +221,7 @@ class MapFrame extends EventTarget {
 
     this.#updatePadding()
     this.#mapbox.fitBounds(bounds, {
-      animate: options.transition && !Util.isDevice('mobile'),
+      animate: (options.transition || true) && !Util.isDevice('mobile'),
     })
   }
 
