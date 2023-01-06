59ee0d2cbabddc29ae72ff14b7709777b67d8764
Devindra <devindra.payment@gmail.com>
2023-01-06 17:24:42 +0000

    Make external registration link work

--- app/assets/javascripts/map/components/event-info.js
+++ app/assets/javascripts/map/components/event-info.js
@@ -42,7 +42,8 @@ function EventInfo() {
           m('a',
             {
               tabindex: 1,
-              href: '#registration',
+              target: (event.registrationMode != 'native' ? '_self' : '_blank'),
+              href: (event.registrationMode != 'native' ? '#registration' : event.registrationUrl),
             },
             m('span.sya-icon.sya-icon--signup'),
             m('span', Util.translate('event.register'))
