1ac814db808c4c9ff8b368da04a375db991ca296
Devindra <devindra.payment@gmail.com>
2023-01-06 18:02:49 +0000

    More details for registrations email

--- config/routes.rb
+++ config/routes.rb
@@ -52,12 +52,12 @@ Rails.application.routes.draw do
     get '(*path)', to: 'application#show'
 
     # For generating helpers
+    get '/event/:event_id', to: 'application#show', as: :event
     scope '(:layer)' do
       get '/country/:country_id', to: 'application#show', as: :country
       get '/region/:region_id', to: 'application#show', as: :region
       get '/area/:area_id', to: 'application#show', as: :area
       get '/venue/:venue_id', to: 'application#show', as: :venue
-      get '/event/:event_id', to: 'application#show', as: :event
     end
   end
 
