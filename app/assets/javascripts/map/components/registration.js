d491a9df7e6d886287dc5e2ccf3b9efbb2401532
Devindra <devindra.payment@gmail.com>
2023-04-12 10:51:39 -0300

    Adjustments to registration form

--- app/assets/javascripts/map/components/registration.js
+++ app/assets/javascripts/map/components/registration.js
@@ -11,36 +11,50 @@ function Registration() {
       const external = event.registrationMode != 'native'
       let registerable = Boolean(event.timing.nextDateTime)
 
+      if (!registerable) {
+        return m('form.sya-registration',
+          m('.sya-registration__header',
+            m('.sya-registration__header__text',
+              m('.sya-registration__header__title', Util.translate('registration.closed'))
+            )
+          )
+        )
+      }
+
+      if (registration) {
+        return m('form.sya-registration.sya-registration--atlas',
+          m(RegistrationConfirm, {
+            event: event,
+            registration: registration,
+            ondismiss: () => { registration = null }
+          })
+        )
+      }
+
       return m(`form.sya-registration.sya-registration--${external ? 'external' : 'atlas'}`,
         m('#registration'),
         m('.sya-registration__header',
           m('.sya-registration__header__text',
             m('.sya-registration__header__title',
-              Util.translate(`registration.${registerable ? 'header' : 'closed'}`)
+              Util.translate('registration.header')
             ),
             m('.sya-registration__header__subtitle', event.timing.description)
           )
         ),
-        registerable ?
-          (registration ?
-            m(RegistrationConfirm, {
-              event: event,
-              registration: registration,
-              ondismiss: () => { registration = null }
-            }) :
-            (external ? m('.sya-registration__external',
-              m('a.sya-registration__external__action', {
-                href: event.registrationUrl,
-                target: '_blank',
-              }, Util.translate(`registration.button.${event.registrationMode}`))
-            ) : m(RegistrationForm, {
-              event: event,
-              onsubmit: response => {
-                registration = response.registration
-                m.redraw()
-              },
-            }))) :
-          null
+      (external ?
+        m('.sya-registration__external',
+          m('a.sya-registration__external__action', {
+            href: event.registrationUrl,
+            target: '_blank',
+          }, Util.translate(`registration.button.${event.registrationMode}`))
+        ) :
+        m(RegistrationForm, {
+          event: event,
+          onsubmit: response => {
+            registration = response.registration
+            m.redraw()
+          },
+        }))
       )
     }
   }
