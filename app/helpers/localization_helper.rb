8735e3254edfb3408075ec7f8fe64aeeb90dc190
Devindra <devindra.payment@gmail.com>
2023-08-19 12:32:46 +0100

    Custom region names for certain countries

--- app/helpers/localization_helper.rb
+++ app/helpers/localization_helper.rb
@@ -1,14 +1,20 @@
 module LocalizationHelper
 
   I18N_KEYS = {
-    OfflineEvent => 'offline_event',
-    OnlineEvent => 'online_event',
+    OfflineEvent => :offline_event,
+    OnlineEvent => :online_event,
   }
 
   def translate_model model, pluralization = :singular, locale: nil
     key = pluralization == :plural ? 'plural' : 'single'
     i18n_key = I18N_KEYS[model] || model.model_name.i18n_key
-    translate(key, scope: [:activerecord, :models, i18n_key], locale: locale)
+    result = translate(key, scope: [:activerecord, :models, i18n_key], locale: locale)
+
+    if i18n_key == :region && @context.try(:country_code)
+      translate(key, scope: [:cms, :region_types, @context.country_code.downcase], locale: locale, default: result)
+    else
+      result
+    end
   end
 
   def translate_model_count model, count = nil
@@ -18,7 +24,13 @@ module LocalizationHelper
     end
 
     i18n_key = I18N_KEYS[model] || model.model_name.i18n_key
-    "#{number_with_delimiter(count)} #{translate(i18n_key, scope: %i[activerecord models], count: count, locale: I18n.locale)}"
+    result = translate(i18n_key, scope: %i[activerecord models], count: count, locale: I18n.locale)
+
+    if i18n_key == :region && @context.try(:country_code)
+      result = translate(i18n_key, scope: [:cms, :region_types, @context.country_code.downcase], locale: locale, default: result)
+    end
+
+    "#{number_with_delimiter(count)} #{result}"
   end
 
   def translate_enum_value model, attr, value = nil
