ace8069448d5c4a42941b72084a544e93dce1012
Devindra <devindra.payment@gmail.com>
2024-07-29 20:52:47 +0100

    Update audit policy

--- app/policies/audit_policy.rb
+++ app/policies/audit_policy.rb
@@ -1,5 +1,13 @@
 class AuditPolicy < DatabasePolicy
 
+  def show?
+    user.administrator? || record.changes.present?
+  end
+
+  def show_technical?
+    user.administrator?
+  end
+
   def new?
     false
   end
