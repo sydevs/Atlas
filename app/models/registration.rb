153f5e36700c896aff1a6e95665a33e2ca8f8c88
Devindra <devindra.payment@gmail.com>
2024-10-29 19:35:51 +0000

    Set reminder field

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -33,6 +33,7 @@ class Registration < ApplicationRecord
   alias default_message_receiver manager
 
   # Callbacks
+  before_create :set_reminder
   after_create :send_registration_notification, if: :immediate_registration_notification?
   before_save :find_or_create_user
 
@@ -40,6 +41,24 @@ class Registration < ApplicationRecord
 
   def starting_date
     starting_at.to_date
+  end
+
+  def set_reminder
+    next_event_at = starting_at < Time.now ? event.next_recurrence_at : starting_at
+    return unless next_event_at.present?
+
+    if event.online?
+      # Online events remind 1 hour before
+      self.next_reminder_at = next_event_at - 1.hour
+    else
+      # Offline events remind 1 day before
+      self.next_reminder_at = next_event_at - 1.day
+    end
+  end
+
+  def set_reminder!
+    set_reminder
+    save!
   end
 
   def subscribe_to! list_id
