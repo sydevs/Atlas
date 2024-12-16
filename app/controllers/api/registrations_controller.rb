663edca4159b88bbec93f4834389eeaa38d60431
Devindra <devindra.payment@gmail.com>
2024-12-16 15:53:29 +1100

    Update registration creation endpoint

--- app/controllers/api/registrations_controller.rb
+++ app/controllers/api/registrations_controller.rb
@@ -1,14 +1,34 @@
 class API::RegistrationsController < API::ApplicationController
 
   def create
+    I18n.locale = params[:locale]&.to_sym || :en
+
+    # Create registrations
     @event = Event.find(params[:event_id])
-    @registration = @event.registrations.create!(registration_params)
+    @user = User.find_by_email(params[:email])
+    @registration = @event.registrations.find_or_initialize_by(user: @user)
+    @registration.assign_attributes(registration_params(@event))
+    @registration.save!
+
+    # Setup emails and subscriptions
+    @registration.subscribe_to!(:registrations)
+    RegistrationMailer.with(registration: @registration).confirmation.deliver_later
+    RegistrationMailer.with(registration: @registration).question.deliver_later if @registration.questions['questions'].present?
+    BrevoAPI.schedule_reminder_email(@registration)
   end
 
-  private
+  protected
 
-    def registration_params
-      params.permit(:event_id, :name, :email, :questions, :starting_at, :time_zone, :locale)
+    def registration_params(event)
+      args = params.permit(:event_id, :questions, :starting_at, :time_zone)
+      time = event.recurrence.starts_at.to_fs(:time).split(':')
+      args[:starting_at] = DateTime.parse(args[:starting_at]).utc.change(hour: time[0].to_i, min: time[1].to_i)
+      args[:user_attributes] = user_params
+      args
+    end
+
+    def user_params
+      params.permit(:name, :email)
     end
 
 end
