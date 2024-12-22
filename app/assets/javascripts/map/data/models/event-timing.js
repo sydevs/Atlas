bfe0c47b68a88aed274ae7b475da9521510c7baa
Devindra <devindra.payment@gmail.com>
2024-12-22 15:34:17 +0800

    Fix time zone display in map

--- app/assets/javascripts/map/data/models/event-timing.js
+++ app/assets/javascripts/map/data/models/event-timing.js
@@ -23,32 +23,34 @@ class EventTiming {
 
   constructor(event) {
     const timing = event.timing
-    const localTimeZone = luxon.DateTime.local().zoneName
 
     this.#online = event.online
     this.#timeZoneDateTime = luxon.DateTime.local().setZone(timing.timeZone)
 
     if (event.type == 'inactive') return
 
-    // Remove 'Z' from all time strings so that we can control the timezone without trouble.
-    timing.firstDate = this.#stripZ(timing.firstDate)
-    timing.lastDate = this.#stripZ(timing.lastDate)
-    timing.upcomingDates = timing.upcomingDates.map(datetime => this.#stripZ(datetime))
-
-    let firstDateTime = luxon.DateTime.fromISO(timing.firstDate, { zone: timing.timeZone })
-    let lastDateTime = timing.lastDate ? luxon.DateTime.fromISO(timing.lastDate, { zone: timing.timeZone }) : null
+    let firstDateTime = luxon.DateTime.fromISO(timing.firstDate)
+    let lastDateTime = timing.lastDate ? luxon.DateTime.fromISO(timing.lastDate) : null
     this.#firstDateTime = firstDateTime
     this.#lastDateTime = lastDateTime
 
-    this.upcomingDateTimes = timing.upcomingDates.map(datetime => luxon.DateTime.fromISO(datetime, { zone: timing.timeZone }))
+    this.upcomingDateTimes = timing.upcomingDates.map(datetime => luxon.DateTime.fromISO(datetime))
     this.isUpcoming = this.upcomingDateTimes.length > 0
 
     if (this.#online) {
+      // Add user timezone for online classes
+      const userTimeZone = luxon.DateTime.local().zoneName
+      firstDateTime = firstDateTime.setZone(userTimeZone)
+      lastDateTime = lastDateTime ? lastDateTime.setZone(userTimeZone) : null
+      this.upcomingDateTimes = this.upcomingDateTimes.map(datetime => datetime.setZone(userTimeZone))
+      this.#timeZoneDateTime = luxon.DateTime.local()
+    } else {
       // Add local timezone for offline classes
+      const localTimeZone = timing.timeZone
       firstDateTime = firstDateTime.setZone(localTimeZone)
       lastDateTime = lastDateTime ? lastDateTime.setZone(localTimeZone) : null
       this.upcomingDateTimes = this.upcomingDateTimes.map(datetime => datetime.setZone(localTimeZone))
-      this.#timeZoneDateTime = luxon.DateTime.local()
+      this.#timeZoneDateTime = luxon.DateTime.fromObject({ zone: localTimeZone })
     }
 
     this.startDate = firstDateTime.toLocaleString({ month: 'long', day: 'numeric' })
